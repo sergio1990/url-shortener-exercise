a8a81e226f63bfd408e41680b9aae76e1683b705
Sergio Gernyak <sergeg1990@gmail.com>
2020-01-02 22:14:57 +0200

    Refactoring by using sinatra error handling

--- api-service/app.rb
+++ api-service/app.rb
@@ -13,43 +13,43 @@ config = Config.new(ENV)
 storage = LinksFileStorage.new(config.storage_file_path, config.logger)
 repository = LinksRepository.new(storage, config.logger)
 
+error RedirectService::UrlNotFoundError do
+  content_type :json
+  status 404
+  { errors: [env['sinatra.error'].message] }.to_json
+end
+
+error JSON::ParserError do
+  content_type :json
+  status 400
+  { errors: [env['sinatra.error'].message] }.to_json
+end
+
 get '/' do
   'Hello world from API!'
 end
 
 post '/urls' do
   content_type :json
-  begin
-    request_payload = JSON.parse(request.body.read)
-    url = request_payload['url']
-    if url.nil? || url == ''
-      status 422
-      { errors: ['The parameter `url` has to be filled!'] }.to_json
-    else
-      service = AddLinkService.new(repository)
-      result = service.call(request_payload['url'])
-      { short_url: "/#{result.short_prefix}", url: result.full_url }.to_json
-    end
-  rescue JSON::ParserError
-    status 400
-    { errors: ['The body has to be a valid JSON object!'] }.to_json
+  request_payload = JSON.parse(request.body.read)
+  url = request_payload['url']
+  if url.nil? || url == ''
+    halt 422, { errors: ['The parameter `url` has to be filled!'] }.to_json
   end
+
+  service = AddLinkService.new(repository)
+  result = service.call(request_payload['url'])
+  { short_url: "/#{result.short_prefix}", url: result.full_url }.to_json
 end
 
 get '/redirect' do
-  begin
-    short_prefix = params['short_prefix'].to_s
-    if short_prefix == ''
-      status 422
-      { errors: ['The parameter `short_prefix` has to be filled!'] }.to_json
-    else
-      service = RedirectService.new(repository)
-      result = service.call(short_prefix)
-      redirect to(result.full_url), 301
-    end
-  rescue RedirectService::UrlNotFoundError => e
-    content_type :json
-    status 404
-    { errors: [e.message] }.to_json
+  short_prefix = params['short_prefix'].to_s
+  if short_prefix == ''
+    status 422
+    halt 422, { errors: ['The parameter `short_prefix` has to be filled!'] }.to_json
   end
+
+  service = RedirectService.new(repository)
+  result = service.call(short_prefix)
+  redirect to(result.full_url), 301
 end
