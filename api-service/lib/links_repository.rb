be1b111bbab0cec5ef188ba02aaeeccc6ac93675
Sergio Gernyak <sergeg1990@gmail.com>
2020-01-02 10:28:49 +0200

    Make the repository object thread-safe

--- api-service/lib/links_repository.rb
+++ api-service/lib/links_repository.rb
@@ -6,20 +6,27 @@ class LinksRepository
   def initialize(storage)
     @storage = storage
     @links_hash = init_links_hash
+    @semaphore = Mutex.new
   end
 
   def all
-    links_hash.values
+    @semaphore.synchronize do
+      links_hash.values
+    end
   end
 
   def get_by_short_prefix(short_prefix)
-    links_hash[short_prefix]
+    @semaphore.synchronize do
+      links_hash[short_prefix]
+    end
   end
 
   def add(link)
-    check_short_prefix_for_existence!(link.short_prefix)
-    links_hash[link.short_prefix] = link
-    storage.persist(links_hash.values)
+    @semaphore.synchronize do
+      check_short_prefix_for_existence!(link.short_prefix)
+      links_hash[link.short_prefix] = link
+      storage.persist(links_hash.values)
+    end
   end
 
   private
