4b55f759540c0437dac822ea43e024b1a6785423
Sergio Gernyak <sergeg1990@gmail.com>
2020-01-02 19:36:44 +0200

    Implement the redirect service

---
+++ api-service/lib/redirect_service.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class RedirectService
+  Result = Struct.new(:full_url)
+  UrlNotFoundError = Class.new(::StandardError)
+
+  def initialize(repository)
+    @repository = repository
+  end
+
+  def call(short_prefix)
+    link = repository.get_by_short_prefix(short_prefix)
+    return Result.new(link.full_url) if link
+
+    raise UrlNotFoundError, "The URL associated with the short prefix `#{short_prefix}` does not exist!"
+  end
+
+  private
+
+  attr_reader :repository
+end
---
+++ api-service/test/lib/redirect_service_test.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+require 'redirect_service'
+
+class RedirectServiceTest < Minitest::Test
+  TEST_FILE = TestFileUtils.tmp_file_path('redirect_service_test.csv')
+
+  def setup
+    TestFileUtils.create_file_with_content(
+      TEST_FILE,
+      'https://farmdrop.com,farmdrop'
+    )
+    @service = RedirectService.new(init_repository(TEST_FILE))
+  end
+
+  def test_when_there_is_an_entry_for_a_given_short_prefix
+    result = service.call('farmdrop')
+
+    assert_equal 'https://farmdrop.com', result.full_url
+  end
+
+  def test_when_a_given_short_prefix_does_not_exist
+    assert_raises RedirectService::UrlNotFoundError do
+      result = service.call('abc123')
+    end
+  end
+
+  private
+
+  attr_reader :service
+
+  def init_repository(file_path)
+    LinksRepository.new(
+      LinksFileStorage.new(file_path)
+    )
+  end
+end
