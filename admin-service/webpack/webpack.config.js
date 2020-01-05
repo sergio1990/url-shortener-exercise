beaadc16e52d3a02159f3a008945b671330f0364
Sergio Gernyak <sergeg1990@gmail.com>
2020-01-05 11:55:24 +0200

    Add bootstrap for styles

--- admin-service/webpack/webpack.config.js
+++ admin-service/webpack/webpack.config.js
@@ -10,14 +10,20 @@ module.exports = {
     path.join(parentDir, 'src', 'index.js')
   ],
   module: {
-    rules: [{
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
-    },{
-      test: /\.less$/,
-      loaders: ["style-loader", "css-loder", "less-loader"]
-    }
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.less$/,
+        loaders: ["style-loader", "css-loader", "less-loader"]
+      },
+      {
+        test: /\.css/,
+        loaders: ["style-loader", "css-loader"]
+      }
     ]
   },
   output: {
