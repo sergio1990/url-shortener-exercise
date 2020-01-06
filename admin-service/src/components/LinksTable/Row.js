2676a81e479e028bd473ae4e61f2ef4306143754
Sergio Gernyak <sergeg1990@gmail.com>
2020-01-06 20:23:58 +0200

    Introduce the config object

--- admin-service/src/components/LinksTable/Row.js
+++ admin-service/src/components/LinksTable/Row.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import config from '../../config';
 
 function Row(props) {
   const { full_url, short_prefix } = props;
+  const short_url = `${config.SHORT_LINK_BASE_URI}${short_prefix}`;
   return (
     <tr>
       <td>{ full_url }</td>
-      <td>{ short_prefix }</td>
+      <td>{ short_url }</td>
       <td>actions...</td>
     </tr>
   );
---
+++ admin-service/src/config.js
@@ -0,0 +1,4 @@
+export default {
+  SHORTENER_API_BASE_URL: "http://localhost:4000/api/",
+  SHORT_LINK_BASE_URI: "http://localhost:4000/"
+};
