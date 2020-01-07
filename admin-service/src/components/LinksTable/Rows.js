49c28e91b12f8ad89b2de389da2e68d10f205430
Sergio Gernyak <sergeg1990@gmail.com>
2020-01-07 22:01:18 +0200

    Render rows depending on loading status

--- admin-service/src/components/LinksTable/Rows.js
+++ admin-service/src/components/LinksTable/Rows.js
@@ -2,15 +2,38 @@ import React, { useContext } from 'react';
 import Row from './Row';
 import { context } from '../LinksContext';
 
+function buildInfoRow(message) {
+  return (
+    <tr>
+      <td colspan="3">
+        <div className="alert alert-light" role="alert">
+          { message }
+        </div>
+      </td>
+    </tr>
+  );
+}
+
+function buildTableBody(content) {
+  return (
+    <tbody>
+      { content }
+    </tbody>
+  );
+}
+
 function Rows() {
   const data = useContext(context);
 
-  const renderedUrls = data === null ? null : data.map(link => <Row key={ link.full_url } {...link} /> )
-  return (
-    <tbody>
-      { renderedUrls }
-    </tbody>
-  );
+  if (data === null) {
+    return buildTableBody(buildInfoRow("A links list is being loading..."));
+  }
+  if (data.length === 0) {
+    return buildTableBody(buildInfoRow("There are no links. You can add a new link using the form above."));
+  }
+
+  const content = data.map(link => <Row key={ link.full_url } {...link} /> )
+  return buildTableBody(content);
 }
 
 export default Rows;
