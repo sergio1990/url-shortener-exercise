48ea615d87b05438faceb1f65fd236187176c7b5
Sergio Gernyak <sergeg1990@gmail.com>
2020-01-09 15:14:09 +0200

    Add basic client-side validation for URL presence

--- admin-service/src/components/AddLinkForm.js
+++ admin-service/src/components/AddLinkForm.js
@@ -5,24 +5,43 @@ import shortenerAPIClient from 'utils/shortener-api';
 function AddLinkForm() {
   const { reload } = useContext(context);
   const [isBadgeShown, setIsBadgeShown] = useState(false);
+  const [isFullUrlFieldInvalid, setFullUrlFieldInvalidState] = useState(false);
 
   let longUrlField = null;
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const longUrl = longUrlField.value;
+    if (longUrl === "") {
+      setFullUrlFieldInvalidState(true);
+      return;
+    }
+
     shortenerAPIClient.shortenLink(longUrl).then(_data => {
       longUrlField.value = null;
       reload();
       setIsBadgeShown(true);
       setTimeout(() => setIsBadgeShown(false), 1500);
+      if (isFullUrlFieldInvalid) { setFullUrlFieldInvalidState(false); }
     });
   };
 
+  let fullUrlFieldClasses = ["form-control", "mb-2", "mr-sm-2", "w-100"];
+  if (isFullUrlFieldInvalid) {
+    fullUrlFieldClasses.push("is-invalid");
+  }
+
   return (
-    <form className="form-inline" onSubmit={ handleSubmit }>
-      <label className="sr-only" htmlFor="long-url">Long URL</label>
-      <input type="text" className="form-control mb-2 mr-sm-2 w-50" id="long-url" placeholder="Long URL" ref={ node => longUrlField = node }/>
+    <form className="mb-4" onSubmit={ handleSubmit }>
+      <div className="form-row">
+        <div className="col-md-12 mb-2">
+          <label className="sr-only" htmlFor="long-url">Long URL</label>
+          <input type="text" className={ fullUrlFieldClasses.join(' ') } id="long-url" placeholder="Long URL" ref={ node => longUrlField = node }/>
+          <div className="invalid-feedback">
+            Please, fill in a full URL to shorten it!
+          </div>
+        </div>
+      </div>
       <button type="submit" className="btn btn-primary mb-2">Shorten URL</button>
       { isBadgeShown && (<span className="ml-2 badge badge-success">shortened successfully</span>)}
     </form>
