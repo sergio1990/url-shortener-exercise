7e311f442b48dadde1658fb898018cab69de4229
Sergio Gernyak <sergeg1990@gmail.com>
2020-01-09 11:48:48 +0200

    Change action icons on mouse over/out events

--- admin-service/src/components/LinksTable/Row.js
+++ admin-service/src/components/LinksTable/Row.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCopy, faArrowAltCircleRight } from '@fortawesome/free-regular-svg-icons';
+import { faCopy as farCopy, faArrowAltCircleRight as farArrowAltCircleRight } from '@fortawesome/free-regular-svg-icons';
+import { faCopy as fasCopy, faArrowAltCircleRight as fasArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
 import config from 'config';
 import { copyStringToClipboard } from 'utils/clipboard';
 
 function Row(props) {
+  const [data, setData] = useState({ isCopyHover: false, isGoToHover: false });
+
   const { full_url, short_prefix } = props;
   const short_url = `${config.SHORT_LINK_BASE_URI}${short_prefix}`;
 
@@ -21,8 +24,19 @@ function Row(props) {
         <a href={ short_url } target="_blank">{ short_url }</a>
       </td>
       <td>
-        <FontAwesomeIcon icon={ faCopy } size="lg" onClick={ onCopyHandler } title="Copy the short link"/>
-        <FontAwesomeIcon icon={ faArrowAltCircleRight } size="lg" className="ml-2" />
+        <FontAwesomeIcon
+          icon={ data.isCopyHover ? fasCopy : farCopy }
+          size="lg"
+          onClick={ onCopyHandler }
+          title="Copy the short link"
+          onMouseOver={ () => setData({ ...data, isCopyHover: true }) }
+          onMouseOut={ () => setData({ ...data, isCopyHover: false }) }/>
+        <FontAwesomeIcon
+          icon={ data.isGoToHover ? fasArrowAltCircleRight : farArrowAltCircleRight }
+          size="lg"
+          onMouseOver={ () => setData({ ...data, isGoToHover: true }) }
+          onMouseOut={ () => setData({ ...data, isGoToHover: false }) }
+          className="ml-2" />
       </td>
     </tr>
   );
