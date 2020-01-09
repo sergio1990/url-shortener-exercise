7e8152e1c986cef3d3efea095e6621eb135ac5a7
Sergio Gernyak <sergeg1990@gmail.com>
2020-01-09 11:15:42 +0200

    Implement copying the short link to clipboard

--- admin-service/src/components/LinksTable/Row.js
+++ admin-service/src/components/LinksTable/Row.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCopy, faArrowAltCircleRight } from '@fortawesome/free-regular-svg-icons';
 import config from 'config';
+import { copyStringToClipboard } from 'utils/clipboard';
 
 function Row(props) {
   const { full_url, short_prefix } = props;
   const short_url = `${config.SHORT_LINK_BASE_URI}${short_prefix}`;
+
+  const onCopyHandler = () => {
+    copyStringToClipboard(short_url);
+  };
+
   return (
     <tr>
       <td>
@@ -15,7 +21,7 @@ function Row(props) {
         <a href={ short_url } target="_blank">{ short_url }</a>
       </td>
       <td>
-        <FontAwesomeIcon icon={ faCopy } size="lg" />
+        <FontAwesomeIcon icon={ faCopy } size="lg" onClick={ onCopyHandler } title="Copy the short link"/>
         <FontAwesomeIcon icon={ faArrowAltCircleRight } size="lg" className="ml-2" />
       </td>
     </tr>
