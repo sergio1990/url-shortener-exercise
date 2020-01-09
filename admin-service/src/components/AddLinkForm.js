7a988584e88a4deef506503919c47bc3f9a702f3
Sergio Gernyak <sergeg1990@gmail.com>
2020-01-09 14:21:43 +0200

    Show badge about successful shorten request

--- admin-service/src/components/AddLinkForm.js
+++ admin-service/src/components/AddLinkForm.js
@@ -1,9 +1,10 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { context } from './LinksContext';
 import shortenerAPIClient from 'utils/shortener-api';
 
 function AddLinkForm() {
   const { reload } = useContext(context);
+  const [isBadgeShown, setIsBadgeShown] = useState(false);
 
   let longUrlField = null;
 
@@ -13,6 +14,8 @@ function AddLinkForm() {
     shortenerAPIClient.shortenLink(longUrl).then(_data => {
       longUrlField.value = null;
       reload();
+      setIsBadgeShown(true);
+      setTimeout(() => setIsBadgeShown(false), 1500);
     });
   };
 
@@ -21,6 +24,7 @@ function AddLinkForm() {
       <label className="sr-only" htmlFor="long-url">Long URL</label>
       <input type="text" className="form-control mb-2 mr-sm-2 w-50" id="long-url" placeholder="Long URL" ref={ node => longUrlField = node }/>
       <button type="submit" className="btn btn-primary mb-2">Shorten URL</button>
+      { isBadgeShown && (<span className="ml-2 badge badge-success">shortened successfully</span>)}
     </form>
   );
 }
